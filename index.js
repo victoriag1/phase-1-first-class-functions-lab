1944e88fe1080bd6915aa515cc249e00bd73b2b2
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-07 21:03:14 -0800

    commit tree 52667b02075b59927ddfad088fc673c8f24b8490

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const firstTwoDrivers = ["Sally", "Bob"]
+const lastTwoDrivers = ["Freddy" , "Claudia"]
+
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(){
+    return firstTwoDrivers
+}
+const returnLastTwoDrivers = function returnLasTwoDrivers(){
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+console.log(selectingDrivers)
+
+function fareDoubler(fare){
+    return fare * 2
+}
+function fareTripler(fare) {
+    return fare *3
+}
+
+function createFareMultiplier(){
+    return function(){
+        return 5*5
+    }
+}
+ 
+function selectDifferentDrivers(){
+    return function(){
+}
+}
+     
+    
